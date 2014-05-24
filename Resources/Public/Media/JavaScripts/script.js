9ebbba43ed34e46d5c70b1d1ff1bc948aa3c1bc3
Kay Strobach <info@kay-strobach.de>
2014-05-24 14:09:56 +0200

    change loading image

--- Resources/Public/Media/JavaScripts/script.js
+++ Resources/Public/Media/JavaScripts/script.js
@@ -2,6 +2,6 @@ $('#main').infinitescroll({
 	navSelector: ".pagebrowser",
 	nextSelector: ".pagebrowser a.next",
 	itemSelector: "#main article.item",
-	loadingImg: "/_Resources/Static/Packages/Planetflow3/Media/Images/ajax-loader.gif"
+	img: "/_Resources/Static/Packages/Planetflow3/Media/Images/ajax-loader-circle.gif"
 
 });
