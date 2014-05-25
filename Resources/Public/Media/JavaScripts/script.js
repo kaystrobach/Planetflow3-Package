db7883e5245f8b441f3351f5a7ea1f351feb3a2e
Kay Strobach <info@kay-strobach.de>
2014-05-25 16:37:08 +0200

    Fix infinitescroll twitter and g+ buttons ...

--- Resources/Public/Media/JavaScripts/script.js
+++ Resources/Public/Media/JavaScripts/script.js
@@ -1,7 +1,13 @@
-$('#main').infinitescroll({
-	navSelector: ".pagebrowser",
-	nextSelector: ".pagebrowser a.next",
-	itemSelector: "#main article.item",
-	img: "/_Resources/Static/Packages/Planetflow3/Media/Images/ajax-loader-circle.gif"
-
-});
+$('#main').infinitescroll(
+	{
+		navSelector: ".pagebrowser",
+		nextSelector: ".pagebrowser a.next",
+		itemSelector: "#main article.item",
+		img: "/_Resources/Static/Packages/Planetflow3/Media/Images/ajax-loader-circle.gif"
+	},function(arrayOfNewElems){
+		window.setTimeout(function() {
+			twttr.widgets.load();
+			gapi.plusone.go();
+		}, 500);
+	}
+);
